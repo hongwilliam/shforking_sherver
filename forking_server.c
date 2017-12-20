e971432eafc8a6e983f1de7ea6d15f5afcc1e085
hongwilliam <willhong20@gmail.com>
2017-12-19 20:36:02 -0500

    filled in subserver functions

--- forking_server.c
+++ forking_server.c
@@ -6,37 +6,40 @@ void subserver(int from_client);
 
 static void sighandler(int signo) {
   if (signo == SIGINT) {
-    remove(WKP;
+    remove(WKP);
     exit(0);
   }
 }
 
 int main() {
+  signal(SIGINT, sighandler);
   int to_client;
   int from_client;
   char downstream[BUFFER_SIZE];
   while (1){
-    from_client = server_handshake(&to_client);
-    while (read (from_client, downstream, BUFFER_SIZE)){
-      printf()
-    }
-
-
-
+    from_client = server_setup();
+    if (fork()){
+      remove(WKP);
+      close(from_client); }
+    else{
+      subserver(from_client); }
   }
-
-
 }
 
 void subserver(int from_client) {
-  int to_client;
-  int from_client;
+  int to_client = server_connect(from_client);
+  char buffer[1000];
+  while (read(from_client, buffer, 1000)){
+    printf("subserver: %d recieved %s \n", getpid(), buffer);
+    process(buffer);
+    write(to_client, buffer, 1000);
+  }
 
 }
 
 //replace
 void process(char * s) {
-
+  return 0;
 
 
 }
